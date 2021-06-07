8155f6f35f63a2f771c6b2b87cd19e26d22b102b
Inosilens <pashin.andrey94@gmail.com>
2021-06-07 21:40:25 +0700

    add button "to top"

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,15 @@
 import { comments } from "./comments";
 
 comments();
+let top = document.querySelector(".toTop");
+
+
+window.addEventListener("scroll", function () {
+  let st = document.documentElement.scrollTop;
+  if (st < 500) {
+    top.classList.add("hiden");
+  } else {
+    top.classList.remove("hiden");
+  }
+  console.log(st);
+});
