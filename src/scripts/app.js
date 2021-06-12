5f70e106b73a9802d6be324f1d3a2acd6c70389b
Inosilens <pashin.andrey94@gmail.com>
2021-06-12 13:32:13 +0700

    just try use js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,13 +3,30 @@ import { comments } from "./comments";
 comments();
 let top = document.querySelector(".toTop");
 
-
 window.addEventListener("scroll", function () {
   let st = document.documentElement.scrollTop;
   if (st < 500) {
-    top.classList.add("hiden");
+    top.style.display = "none";
   } else {
-    top.classList.remove("hiden");
+    top.style.display = "block";
   }
   console.log(st);
 });
+let topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+let btn = document.querySelector(".toTop");
+
+btn.addEventListener("click", topFunction);
+
+
+let pancakesImg  = document.querySelector(".pancaaker__img")
+let pancaakesMenu = document.querySelector(".pancaakes__menu")
+let setHeight = () =>
+{
+  pancakesImg.style.height = pancaakesMenu.clientHeight + "px"
+
+}
+setHeight()
